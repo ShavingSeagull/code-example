8537284f4bb925196758adc9154b33887eb81abc
Scott Kipp <scott.ecfx@gmail.com>
2020-02-27 20:41:17 +0000

    Add files via upload

--- engineer_example.py
+++ engineer_example.py
@@ -2,15 +2,13 @@
 def sum_interval(array, start, stop):
     if stop >= len(array): 
         raise IndexError("The stop index is out of range")
-    
+
     total = 0
 
     for num in range(start, stop + 1):
         total += array[num]
     
     return total
-
-print(sum_interval([10,20,30,40,50], 4, 5))
 
 # O(n^2) time | O(n) space
 def rev_cumulutive_sum(array):
